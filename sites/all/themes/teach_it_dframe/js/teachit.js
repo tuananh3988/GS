a9ed27b7c7e6261dc059ffd9aef1b2f1a22f4c21
tuananh3988 <tuananh8893@yahoo.com>
2013-09-26 02:38:11 +0700

    home

--- sites/all/themes/teach_it_dframe/js/teachit.js
+++ sites/all/themes/teach_it_dframe/js/teachit.js
@@ -1,6 +1,18 @@
 (function ($) {
 
     $(document).ready(function () {
+      var url = String(window.location);
+      
+      if(url.indexOf('/recycle') > 0) {
+        $('.logo-shop a').addClass('not-active');
+        $('.logo-consign a').addClass('not-active');
+      }
+      
+      if(url.indexOf('/consign') > 0) {
+        $('.logo-shop a').addClass('not-active');
+        $('.logo-recycle a').addClass('not-active');
+      }
+      
         //Mobile Navigation
       $('#zone-menu .menu').mobileMenu({
         defaultText: 'Navigate to...',
